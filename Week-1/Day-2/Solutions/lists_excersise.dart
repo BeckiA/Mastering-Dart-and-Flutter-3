13fc27427acf5a8de7444d429ca584f7037e97fc
becki <berekeetabera@gmail.com>
2023-09-08 15:53:29 +0300

    Day -2 Excersice on List

---
+++ Week-1/Day-2/Solutions/lists_excersise.dart
@@ -0,0 +1,11 @@
+void main() {
+//  Show two different methods of
+// grabbing the last item in the list
+  var myList = ['A', 'B', 'C', 'D', 'E'];
+
+  // Method - 1
+  print(myList.last);
+
+  // Method - 2
+  print(myList[myList.length - 1]);
+}
