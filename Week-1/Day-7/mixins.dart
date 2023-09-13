7477d8fff4a8c5d202ac05f6d51f67ff3b6d3e71
becki <berekeetabera@gmail.com>
2023-09-13 10:51:52 +0300

    Getting startred with Flutter

--- Week-1/Day-7/mixins.dart
+++ Week-1/Day-7/mixins.dart
@@ -1,4 +1,5 @@
 // mixins: Generally mixins are a class of methods for dart(We use mixins to write methods)
+// Mixins are a way to reuse a class's code in multiple class hierarchies.
 mixin WalkMixin {
   void walk() {
     print("I can walk");
