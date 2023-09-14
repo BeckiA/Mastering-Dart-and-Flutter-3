41035ac11cf5e8338cc673530e4df3e913d225a7
becki <berekeetabera@gmail.com>
2023-09-14 11:22:25 +0300

    Day-8 Practice on Stateless Widget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,13 +2,16 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp(
-    theme:
-        ThemeData(primaryColor: Colors.blueGrey, brightness: Brightness.dark),
-    home: Scaffold(
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('AppBar Title'),
         ),
-        body: const Center(child: Text("Hello World"))),
-  ));
+        body: const Center(child: Text("Hello World")));
+  }
 }
