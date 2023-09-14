cc6214ce5fd39b2ea8a1028863821c5741806d71
becki <berekeetabera@gmail.com>
2023-09-14 12:23:39 +0300

    Day-8 Working on Background Container

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,40 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Centered Column Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-              ),
-            ],
-          ),
-        ),
+void main() => runApp(
+      MaterialApp(
+        home: MyWidget(),
       ),
     );
-  }
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(color: Colors.red),
+      );
 }
