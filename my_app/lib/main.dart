6d40e7bd8b21ff78eee17947b21191a7c14f6acb
becki <berekeetabera@gmail.com>
2023-09-16 13:33:38 +0300

    Day-9 Exercising on the AlertDialog Feature

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,42 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
+    return MaterialApp(title: 'Dialog App', home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // Variable Initalization goes here
-  String _myText = '';
+  String _textInput = '';
+  void _showInputDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController controller = TextEditingController();
+
+        return AlertDialog(
+          title: const Text('Input Your Text'),
+          content: TextField(
+            controller: controller,
+            decoration: const InputDecoration(hintText: 'Your Text Goes Here'),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () => setState(() {
+                      _textInput = controller.text;
+                      Navigator.of(context).pop();
+                    }),
+                child: const Text('Save'))
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("The Home Page"),
-      ),
+      appBar: AppBar(title: const Text('Brand New Application')),
       body: Center(
-        child: TextField(
-          decoration: const InputDecoration(hintText: "Your Input Goes Here"),
-          onChanged: (value) => setState(() {
-            _myText = value;
-          }),
-        ),
+        child: Text(_textInput.isEmpty ? "Please Input Your Text" : _textInput),
       ),
-      bottomSheet: Container(
-          alignment: Alignment.bottomCenter,
-          height: 50,
-          child: Text('You have typed: $_myText')),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _showInputDialog, child: const Icon(Icons.add)),
     );
   }
 }
