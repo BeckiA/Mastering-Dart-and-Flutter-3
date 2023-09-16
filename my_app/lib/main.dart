efd3347e0579055238bad64c4048a4c7cce1d15c
becki <berekeetabera@gmail.com>
2023-09-16 14:10:43 +0300

    Day-9 Exersicing on ListView.build()

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,64 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'List View App',
-    home: MyApp(),
-  ));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Application'),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Person One'),
-            subtitle: const Text('email@john.com'),
-            trailing: const Icon(Icons.arrow_forward),
-            // ignore: avoid_print
-            onTap: () => print("List is pressed"),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Person One'),
-            subtitle: const Text('email@john.com'),
-            trailing: const Icon(Icons.arrow_forward),
-            // ignore: avoid_print
-            onTap: () => print("List is pressed"),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Person One'),
-            subtitle: const Text('email@john.com'),
-            trailing: const Icon(Icons.arrow_forward),
-            // ignore: avoid_print
-            onTap: () => print("List is pressed"),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Person One'),
-            subtitle: const Text('email@john.com'),
-            trailing: const Icon(Icons.arrow_forward),
-            // ignore: avoid_print
-            onTap: () => print("List is pressed"),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: const Text('Person One'),
-            subtitle: const Text('email@john.com'),
-            trailing: const Icon(Icons.arrow_forward),
-            // ignore: avoid_print
-            onTap: () => print("List is pressed"),
-          )
-        ],
+    // Create a list of items to display in the list view
+    final items = ['item1', 'item2', 'item3'];
+
+    return MaterialApp(
+      title: 'Flutter ListView.builder Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView.builder Example'),
+        ),
+        body: ListView.builder(
+          // Use the itemCount property to specify the number of items in the list
+          itemCount: items.length,
+          // Use the itemBuilder property to specify how each item should be displayed
+          itemBuilder: (context, index) {
+            // Get the item at the specified index
+            final item = items[index];
+            // Return a ListTile widget that displays the item's text
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                // Do something when the list tile is tapped
+                print('You just clicked on $item');
+              },
+            );
+          },
+        ),
       ),
     );
   }
