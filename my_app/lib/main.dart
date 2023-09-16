c21038428367225356dcfa9d609cdf6f4e5dc20b
becki <berekeetabera@gmail.com>
2023-09-16 14:02:30 +0300

    Day-9 Exerscing on ListView and ListTile

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,61 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp(),
-  );
+  runApp(const MaterialApp(
+    title: 'List View App',
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Dialog App', home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _textInput = '';
-  void _showInputDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        TextEditingController controller = TextEditingController();
-
-        return AlertDialog(
-          title: const Text('Input Your Text'),
-          content: TextField(
-            controller: controller,
-            decoration: const InputDecoration(hintText: 'Your Text Goes Here'),
-          ),
-          actions: [
-            TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Cancel')),
-            TextButton(
-                onPressed: () => setState(() {
-                      _textInput = controller.text;
-                      Navigator.of(context).pop();
-                    }),
-                child: const Text('Save'))
-          ],
-        );
-      },
-    );
-  }
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Brand New Application')),
-      body: Center(
-        child: Text(_textInput.isEmpty ? "Please Input Your Text" : _textInput),
+      appBar: AppBar(
+        title: const Text('My Application'),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _showInputDialog, child: const Icon(Icons.add)),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Person One'),
+            subtitle: const Text('email@john.com'),
+            trailing: const Icon(Icons.arrow_forward),
+            // ignore: avoid_print
+            onTap: () => print("List is pressed"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Person One'),
+            subtitle: const Text('email@john.com'),
+            trailing: const Icon(Icons.arrow_forward),
+            // ignore: avoid_print
+            onTap: () => print("List is pressed"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Person One'),
+            subtitle: const Text('email@john.com'),
+            trailing: const Icon(Icons.arrow_forward),
+            // ignore: avoid_print
+            onTap: () => print("List is pressed"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Person One'),
+            subtitle: const Text('email@john.com'),
+            trailing: const Icon(Icons.arrow_forward),
+            // ignore: avoid_print
+            onTap: () => print("List is pressed"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text('Person One'),
+            subtitle: const Text('email@john.com'),
+            trailing: const Icon(Icons.arrow_forward),
+            // ignore: avoid_print
+            onTap: () => print("List is pressed"),
+          )
+        ],
+      ),
     );
   }
 }
