3378a86ac6055c2ef6e280aa236413675331015e
becki <berekeetabera@gmail.com>
2023-09-17 08:05:28 +0300

    Day10 Working With TODO List App

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,37 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: "To-Do List App",
+    home: MyHomePage(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Initalization of to do lists with mock items
+  List<String> _todos = ['Task1', 'Task2', 'Task3'];
+
+  void _todo() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        // Variable Initalization for the todo List Item which is going to be added
+        String todoItem = '';
+        return AlertDialog(
+          title: const Text("Add Your TODO's Here"),
+          content: TextField(
+            decoration: const InputDecoration(hintText: "Input Your Task Here"),
+            onChanged: (value) => setState(() {
+              todoItem = value;
+            }),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _todos.add(todoItem);
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Submit'))
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Create a list of items to display in the list view
-    final items = ['item1', 'item2', 'item3'];
-
-    return MaterialApp(
-      title: 'Flutter ListView.builder Example',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListView.builder Example'),
-        ),
-        body: ListView.builder(
-          // Use the itemCount property to specify the number of items in the list
-          itemCount: items.length,
-          // Use the itemBuilder property to specify how each item should be displayed
+    return Scaffold(
+      appBar: AppBar(title: const Text("My TODO List")),
+      body: ListView.builder(
+          itemCount: _todos.length,
           itemBuilder: (context, index) {
-            // Get the item at the specified index
-            final item = items[index];
-            // Return a ListTile widget that displays the item's text
+            final todo = _todos[index]; // The current to-do item
+
             return ListTile(
-              title: Text(item),
-              onTap: () {
-                // Do something when the list tile is tapped
-                print('You just clicked on $item');
-              },
+              leading: const Icon(Icons.task),
+              title: Text(todo,
+                  style: TextStyle(
+                      decoration: todo.startsWith('-')
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none)),
+              onTap: () => setState(() {
+                if (todo.startsWith('-')) {
+                  _todos[index] = todo.substring(
+                      2); // If the item is already checked, uncheck it
+                } else {
+                  _todos[index] = '- $todo'; // Otherwise, check it
+                }
+              }),
             );
-          },
-        ),
-      ),
+          }),
+      floatingActionButton:
+          FloatingActionButton(onPressed: _todo, child: const Icon(Icons.add)),
     );
   }
 }
