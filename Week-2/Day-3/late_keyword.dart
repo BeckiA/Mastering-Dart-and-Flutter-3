8c5c6a431b6d5c6cdef16a7059c066cf273e0ae6
becki <berekeetabera@gmail.com>
2023-09-17 10:20:08 +0300

    Day10 Exersicing on the usage of late keyword

---
+++ Week-2/Day-3/late_keyword.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // Initalize the variable
+  // But I'll Assign it later
+  late String name;
+
+  // Assignment
+  name = 'Becki';
+
+  // Now use it
+  print(name);
+}
