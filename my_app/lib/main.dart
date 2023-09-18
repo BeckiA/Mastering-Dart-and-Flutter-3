43934afaf20d371842473f37681c54820e41f76b
becki <berekeetabera@gmail.com>
2023-09-18 14:37:32 +0300

    Day-11 Working on the Navigation Across Screens on the Todo List App

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,40 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(title: 'My Flutter', home: FirstScreen()));
+class Todo {
+  final String title;
+  final String description;
+  Todo({required this.title, required this.description});
 }
 
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Screen One"),
-        ),
-        //Center Widget with Elevated Button On It To Navigate to the Second Screen
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SecondScreen(),
-              )),
-          child: const Text('GO TO THE SECOND SCREEN'),
-        )));
-  }
-}
+List<Todo> _todoList = [
+  Todo(title: "Pray and BS", description: "Study Exodus Chapter 6 and 7"),
+  Todo(
+      title: 'Mastering Flutter and Dart',
+      description: 'Try to finish the Navigation module'),
+  Todo(title: 'Make Calls', description: 'Try to catch Bereket and Mr.Gizate'),
+];
 
-class SecondScreen extends StatelessWidget {
+void main() => runApp(MaterialApp(
+      title: 'Todo App',
+      home: TodoScreen(todos: _todoList),
+    ));
+
+class TodoScreen extends StatelessWidget {
+  TodoScreen({Key? key, required this.todos}) : super(key: key);
+
+  List<Todo> todos;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Screen Two"),
+        title: const Text('Tasks TODO'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.task),
+            title: Text(todos[index].title),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                )),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  DetailScreen({Key? key, required this.todo}) : super(key: key);
+
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
       ),
       body: Center(
-        child: ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('GO TO THE FIRST SCREEN')),
+        child: Text(todo.description),
       ),
     );
   }
