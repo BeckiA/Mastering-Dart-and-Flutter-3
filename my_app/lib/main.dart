925349b16da19ea1d1941d12a5b852bbf7be04f3
becki <berekeetabera@gmail.com>
2023-09-18 09:19:19 +0300

    Day11 Exersicing on the Navigator Widget

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,80 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "To-Do List App",
-    home: MyHomePage(),
-  ));
+  runApp(MaterialApp(title: 'My Flutter', home: FirstScreen()));
 }
 
-class MyHomePage extends StatefulWidget {
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // Initalization of to do lists with mock items
-  List<String> _todos = ['Task1', 'Task2', 'Task3'];
-
-  void _todo() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        // Variable Initalization for the todo List Item which is going to be added
-        String todoItem = '';
-        return AlertDialog(
-          title: const Text("Add Your TODO's Here"),
-          content: TextField(
-            decoration: const InputDecoration(hintText: "Input Your Task Here"),
-            onChanged: (value) => setState(() {
-              todoItem = value;
-            }),
-          ),
-          actions: [
-            TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: const Text('Cancel')),
-            TextButton(
-                onPressed: () {
-                  setState(() {
-                    _todos.add(todoItem);
-                  });
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Submit'))
-          ],
-        );
-      },
-    );
-  }
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("My TODO List")),
-      body: ListView.builder(
-          itemCount: _todos.length,
-          itemBuilder: (context, index) {
-            final todo = _todos[index]; // The current to-do item
+        appBar: AppBar(
+          title: const Text("Screen One"),
+        ),
+        //Center Widget with Elevated Button On It To Navigate to the Second Screen
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SecondScreen(),
+              )),
+          child: const Text('GO TO THE SECOND SCREEN'),
+        )));
+  }
+}
 
-            return ListTile(
-              leading: const Icon(Icons.task),
-              title: Text(todo,
-                  style: TextStyle(
-                      decoration: todo.startsWith('-')
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none)),
-              onTap: () => setState(() {
-                if (todo.startsWith('-')) {
-                  _todos[index] = todo.substring(
-                      2); // If the item is already checked, uncheck it
-                } else {
-                  _todos[index] = '- $todo'; // Otherwise, check it
-                }
-              }),
-            );
-          }),
-      floatingActionButton:
-          FloatingActionButton(onPressed: _todo, child: const Icon(Icons.add)),
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Screen Two"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('GO TO THE FIRST SCREEN')),
+      ),
     );
   }
 }
