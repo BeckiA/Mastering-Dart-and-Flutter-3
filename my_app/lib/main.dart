b6c527cbb6c339e1911ef13098c5a9cf336f0ed0
becki <berekeetabera@gmail.com>
2023-09-19 12:41:04 +0300

    Day-12 Working on Grabbing Values from TextFields Using Controllers

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,66 +3,74 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  String appTItle = 'Form Styling Demo';
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: appTItle,
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(appTItle),
-          ),
-          body: HomeScreen(),
-        ));
+      title: 'Form Styling Demo',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const CustomForm(),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class CustomForm extends StatefulWidget {
+  const CustomForm({super.key});
+  @override
+  _CustomFormState createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              height: 16,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'TEXT FIELD',
-                  icon: Icon(
-                    Icons.person,
-                    size: 30,
-                  )),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'TEXT FORM FIELD',
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal,
-                  padding: const EdgeInsets.all(8)),
-              child: const Text('ELEVATED BUTTON'),
-            )
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grabbing Values from Text Fields'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(children: [
+          TextField(
+            controller: userNameController,
+            decoration: const InputDecoration(hintText: 'Username'),
+          ),
+          TextField(
+            controller: passwordController,
+            decoration: const InputDecoration(hintText: 'Email'),
+          )
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Displaying the Values'),
+                content: Column(
+                  children: [
+                    Text("Username: ${userNameController.text}"),
+                    Text("Password: ${passwordController.text}"),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        tooltip: "Get Your Inputs",
+        child: const Icon(Icons.text_fields),
       ),
     );
   }
