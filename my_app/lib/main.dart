71af3dcbe8120c6aad9bec0e2faa1b4782079043
becki <berekeetabera@gmail.com>
2023-09-19 11:19:20 +0300

    Day-12 Exersice on Form Stylings

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,118 +1,68 @@
 import 'package:flutter/material.dart';
 
-class Todo {
-  final String title;
-  final String description;
+void main() => runApp(MyApp());
 
-  Todo({required this.title, required this.description});
-}
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
-List<Todo> _todos = [
-  Todo(title: "Pray and BS", description: "Study Exodus Chapter 6 and 7"),
-  Todo(
-      title: 'Mastering Flutter and Dart',
-      description: 'Try to finish the Navigation module'),
-  Todo(title: 'Make Calls', description: 'Try to catch Bereket and Mr.Gizate'),
-];
+  String appTItle = 'Form Styling Demo';
 
-void main() => runApp(MaterialApp(
-      title: 'The Todo App',
-      home: TodoScreen(todos: _todos),
-    ));
-
-class TodoScreen extends StatefulWidget {
-  const TodoScreen({Key? key, required this.todos}) : super(key: key);
-  final List<Todo> todos;
-
-  _TodoScreenState createState() => _TodoScreenState();
-}
-
-class _TodoScreenState extends State<TodoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My TODOs'),
-      ),
-      body: ListView.builder(
-          itemCount: _todos.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: const Icon(Icons.task),
-              title: Text(_todos[index].title),
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DetailScreen(
-                            todo: _todos[index],
-                          ))),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final Todo? newTodo = await showDialog(
-            context: context,
-            builder: (context) {
-              String? title;
-              String? description;
-
-              return AlertDialog(
-                title: const Text('Add New Task'),
-                content: Column(
-                  children: [
-                    TextField(
-                      onChanged: (value) => title = value,
-                      decoration: const InputDecoration(hintText: 'Task Title'),
-                    ),
-                    TextField(
-                      onChanged: (value) => description = value,
-                      decoration:
-                          const InputDecoration(hintText: 'Task Description'),
-                    )
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text('Cancel')),
-                  TextButton(
-                      onPressed: () {
-                        if (title != null && description != null) {
-                          return Navigator.pop(context,
-                              Todo(title: title!, description: description!));
-                        }
-                      },
-                      child: const Text('Save'))
-                ],
-              );
-            },
-          );
-
-          if (newTodo != null) {
-            setState(() {
-              _todos.add(newTodo);
-            });
-          }
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+    return MaterialApp(
+        title: appTItle,
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(appTItle),
+          ),
+          body: HomeScreen(),
+        ));
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  final Todo todo;
-  DetailScreen({Key? key, required this.todo}) : super(key: key);
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(todo.title),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Center(child: Text(todo.description)),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  hintText: 'TEXT FIELD',
+                  icon: Icon(
+                    Icons.person,
+                    size: 30,
+                  )),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'TEXT FORM FIELD',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  padding: const EdgeInsets.all(8)),
+              child: const Text('ELEVATED BUTTON'),
+            )
+          ],
+        ),
       ),
     );
   }
