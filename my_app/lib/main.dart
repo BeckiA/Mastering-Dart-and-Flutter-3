721ff5efa3fa252ed318aafa28cf3fd9d3f890c8
becki <berekeetabera@gmail.com>
2023-09-20 12:24:30 +0300

    Day-12 Exersice on Validating forms with GlobalKey and Storing the Values

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,77 +1,95 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final appTitle = 'Form Demo App';
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Form Styling Demo',
+      title: appTitle,
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: const CustomForm(),
+      home: Scaffold(
+        appBar: AppBar(title: Text(appTitle)),
+        body: const LoginForm(),
+      ),
     );
   }
 }
 
-class CustomForm extends StatefulWidget {
-  const CustomForm({super.key});
-  @override
-  _CustomFormState createState() => _CustomFormState();
+class LoginForm extends StatefulWidget {
+  const LoginForm({Key? key}) : super(key: key);
+  _LoginFormState createState() => _LoginFormState();
 }
 
-class _CustomFormState extends State<CustomForm> {
-  final userNameController = TextEditingController();
-  final passwordController = TextEditingController();
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   void dispose() {
-    userNameController.dispose();
-    passwordController.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Grabbing Values from Text Fields'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [
-          TextField(
-            controller: userNameController,
-            decoration: const InputDecoration(hintText: 'Username'),
+    return Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                    hintText: 'Username',
+                    icon: Icon(
+                      Icons.person,
+                      size: 25,
+                    )),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Your Text Here';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                    hintText: 'Password',
+                    icon: Icon(
+                      Icons.key,
+                      size: 25,
+                    )),
+                validator: (value) {
+                  if (value!.length < 6) {
+                    return 'Password Must be 6 Digit';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("Username: ${_usernameController.text}");
+                      print("Password: ${_passwordController.text}");
+                    }
+                  },
+                  child: const Text('LOGIN'))
+            ],
           ),
-          TextField(
-            controller: passwordController,
-            decoration: const InputDecoration(hintText: 'Email'),
-          )
-        ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text('Displaying the Values'),
-                content: Column(
-                  children: [
-                    Text("Username: ${userNameController.text}"),
-                    Text("Password: ${passwordController.text}"),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-        tooltip: "Get Your Inputs",
-        child: const Icon(Icons.text_fields),
-      ),
-    );
+        ));
   }
 }
