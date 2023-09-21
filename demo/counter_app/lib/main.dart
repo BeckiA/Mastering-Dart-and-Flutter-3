4ecb9f2bbef52d948728aa7ce8236baf125fd4b5
becki <berekeetabera@gmail.com>
2023-09-21 22:18:38 +0300

    Day-13 Practicing on How to Read and Write data on the app

---
+++ demo/counter_app/lib/main.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'dart:io';
+
+// void main() => runApp(MaterialApp(
+//       title: 'Counter Demo',
+//       home: FlutterDemo(storage: counter),
+//     ));
+
+// Future<String> get _localPath async {
+//   final directory = await getApplicationDocumentsDirectory();
+
+//   return directory.path;
+// }
+
+// Future<File> get _localFile async{
+//   final path = await _localPath
+// }
+// class FlutterDemo extends StatefulWidget {}
